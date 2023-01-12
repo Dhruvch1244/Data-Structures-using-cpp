e529a156abff87d5f9d633c90ae5b69c43d26731
Dhruvch1244 <dr3515@srmist.edu.in>
2023-01-12 21:11:06 +0530

    new file:   sorting/bubblesort.cpp

---
+++ sorting/bubblesort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int>arr = {2,5,1,4,7};
+    int i,j;
+    for(i = 0;i<arr.size()-1;i++){
+        for(j = 0;j<arr.size()-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int x = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = x;
+            }
+        }
+    }
+    for(int k = 0;k<arr.size();k++){
+        cout<<arr[k]<<" ";
+    }
+    return 0;
+}
