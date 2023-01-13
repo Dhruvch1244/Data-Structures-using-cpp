1450dd9fecc8a367173d370fbc1a57d6b1cd436d
Dhruvch1244 <dr3515@srmist.edu.in>
2023-01-13 18:36:38 +0530

    new file:   sorting/selectionsort.cpp

---
+++ sorting/selectionsort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int>arr = {2,5,1,4,7};
+    int i,j;
+    for(i = 0;i<arr.size();i++){
+        int x = i;
+        for(j = 0;j<i;j++){
+            if(arr[j]>arr[i]){
+            x = j;  
+            swap(arr[i],arr[x]);
+
+            }
+        }
+    }
+    for(int k = 0;k<arr.size();k++){
+        cout<<arr[k]<<" ";
+    }
+    return 0;
+}
