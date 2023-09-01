e0742947d0d22e4f5444b1f15f5da01a9738d75c
Oumuamua <aryanandroid26@gmail.com>
2023-09-01 20:02:11 +0530

    Added header files

---
+++ Heap/MaxHeap.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class MaxHeap {
+    // valid index are from 1 to n
+    //  data[0] is -1, an invalid node
+    vector<int> data;
+
+    void _heapify(unsigned int pos, unsigned int size);
+
+  public:
+    MaxHeap();
+    MaxHeap(vector<int> d);
+
+    inline unsigned int getSize() { return data.size() - 1; }
+    void heapify(unsigned int pos) { _heapify(pos, data.size()); }
+
+    void heapsort();
+    void buildMaxHeap();
+
+    void printHeap();
+};
